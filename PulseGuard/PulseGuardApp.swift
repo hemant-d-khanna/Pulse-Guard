03a826c9ef998cd96e963a20a94f295468743779
Hemant Khanna <hemant.d.khanna@gmail.com>
2026-06-12 12:49:02 -0700

    Initial Commit

---
+++ PulseGuard/PulseGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseGuardApp.swift
+//  PulseGuard
+//
+//  Created by Hemant Khanna on 6/12/26.
+//
+
+import SwiftUI
+
+@main
+struct PulseGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
